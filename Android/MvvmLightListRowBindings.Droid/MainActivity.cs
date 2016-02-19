82153ceb2b7dc3f9103b8f9d7cfec40ccf2a11a1
Mark Allibone <mark.allibone@noser.com>
2016-02-19 23:57:07 +0100

    Adds initial project template

---
+++ Android/MvvmLightListRowBindings.Droid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MvvmLightListRowBindings.Droid
+{
+    [Activity(Label = "MvvmLightListRowBindings.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
+
