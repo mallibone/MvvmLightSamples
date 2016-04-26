73daca60c7caa4f8a472ada4c6bb7b9669956883
Mark Allibone <mark.allibone@noser.com>
2016-04-26 07:50:05 +0200

    Updates XTC version to 1.3.x

--- MvvmLightBindings/MvvmLightBindings.UITest/Tests.cs
+++ MvvmLightBindings/MvvmLightBindings.UITest/Tests.cs
@@ -29,6 +29,7 @@ namespace MvvmLightBindings.UITest
         public void AppLaunches()
         {
             app.Screenshot("First screen.");
+            app.Repl();
         }
     }
 }
--- MvvmLightBindings/MvvmLightBindings/App.cs
+++ MvvmLightBindings/MvvmLightBindings/App.cs
@@ -17,21 +17,6 @@ namespace MvvmLightBindings
 
         public App()
         {
-            // The root page of your application
-            //MainPage = new ContentPage
-            //{
-            //    Content = new StackLayout
-            //    {
-            //        VerticalOptions = LayoutOptions.Center,
-            //        Children = {
-            //            new Label {
-            //                XAlign = TextAlignment.Center,
-            //                Text = "Welcome to Xamarin Forms!"
-            //            }
-            //        }
-            //    }
-            //};
-
             MainPage = new MainView();
         }
 
